e4c329610bd2f631b6a05e41f06114e961c9806d
metaboulie <eugeneheiner14@gmail.com>
2025-03-02 19:59:02 +0800

    Add configuration for the main `/.config/` folder

---
+++ .config/nvim/lua/eugene/plugins/oil.lua
@@ -0,0 +1,62 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			CustomOilBar = function()
+				local path = vim.fn.expand("%")
+				path = path:gsub("oil://", "")
+
+				return "  " .. vim.fn.fnamemodify(path, ":.")
+			end
+
+			require("oil").setup({
+				columns = { "icon" },
+				delete_to_trash = true,
+				-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+				skip_confirm_for_simple_edits = true,
+				-- Set to true to watch the filesystem for changes and reload oil
+				watch_for_changes = true,
+				keymaps = {
+					["g?"] = { "actions.show_help", mode = "n" },
+					["<CR>"] = "actions.select",
+					["<C-s>"] = { "actions.select", opts = { vertical = true } },
+					["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+					["<C-t>"] = { "actions.select", opts = { tab = true } },
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = { "actions.close", mode = "n" },
+					["<C-l>"] = "actions.refresh",
+					["-"] = { "actions.parent", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["`"] = { "actions.cd", mode = "n" },
+					["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+					["gs"] = { "actions.change_sort", mode = "n" },
+					["gx"] = "actions.open_external",
+					["g."] = { "actions.toggle_hidden", mode = "n" },
+					["g\\"] = { "actions.toggle_trash", mode = "n" },
+					["<M-h>"] = "actions.select_split",
+				},
+				win_options = {
+					winbar = "%{v:lua.CustomOilBar()}",
+				},
+				view_options = {
+					show_hidden = true,
+					is_always_hidden = function(name, _)
+						local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
+						return vim.tbl_contains(folder_skip, name)
+					end,
+				},
+				-- Customize the highlight group for the file name
+				highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+					return nil
+				end,
+			})
+
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+	},
+}
