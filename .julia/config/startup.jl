508feac2a7c6ced03a22c62a2c33a49bda502662
metaboulie <eugeneheiner14@gmail.com>
2025-03-02 20:07:35 +0800

    Add configuration for julia

---
+++ .julia/config/startup.jl
@@ -0,0 +1,64 @@
+using Pkg
+Pkg.precompile()
+
+#=
+Using Revise.jl
+- https://timholy.github.io/Revise.jl/stable/config/
+=#
+try
+    using Revise
+    using DispatchDoctor: @stable
+    using BenchmarkTools
+    using TimerOutputs
+    using JET
+    using Pluto
+catch e
+    @warn "Error initializing Revise" exception = (e, catch_backtrace())
+end
+
+#=
+Plugin link:
+- https://juliaci.github.io/PkgTemplates.jl/stable/user/#Plugins
+
+How to use:
+julia> t = Template();
+julia> t("PkgName")
+
+* if a plugin is commented with `template`, then it can be customed with a template
+- https://juliaci.github.io/PkgTemplates.jl/stable/user/#Custom-Template-Files
+=#
+function template()
+    @eval begin
+        using PkgTemplates
+        PkgTemplates.Template(;
+            dir="~/dev/julia",
+            plugins=[
+                PkgTemplates.ProjectFile(; version=v"0.1.0-DEV"),
+                PkgTemplates.SrcDir(;), # template
+                PkgTemplates.Tests(; # template
+                    project=true,
+                    aqua=true,
+                    aqua_kwargs=(ambiguities=false,),
+                    jet=true,
+                ),
+                PkgTemplates.Readme(;), # template
+                PkgTemplates.License(; name="MIT"),
+                PkgTemplates.Git(;),
+                PkgTemplates.GitHubActions(; osx=true, windows=true, x86=true), # template
+                PkgTemplates.GitHubActions(;
+                    file=joinpath(@__DIR__, "Format.yml"), destination="Format.yml"
+                ),
+                PkgTemplates.CompatHelper(;), # gh action
+                PkgTemplates.TagBot(;), # gh action
+                PkgTemplates.Dependabot(;), # gh action
+                PkgTemplates.Codecov(;),
+                # https://documenter.juliadocs.org/stable/man/hosting/#Hosting-Documentation
+                PkgTemplates.Documenter{GitHubActions}(;), # template gh action
+                PkgTemplates.BlueStyleBadge(;),
+                # PkgTemplates.Develop(),
+                PkgTemplates.Citation(; readme=true), # template
+                PkgTemplates.Formatter(; style="blue"), # template
+            ],
+        )
+    end
+end
