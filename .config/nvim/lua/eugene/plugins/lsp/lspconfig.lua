43691ccf611419e31d4fe1cf9208fb2c7f3be328
metaboulie <eugeneheiner14@gmail.com>
2025-03-03 18:36:18 +0800

    fix(luals): fix the error: "Your workspace is set to 'x'. Lua language server refused to load this dir."
    reference: https://github.com/LuaLS/lua-language-server/issues/2975

--- .config/nvim/lua/eugene/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/eugene/plugins/lsp/lspconfig.lua
@@ -114,14 +114,27 @@ return {
 				})
 			end,
 			["lua_ls"] = function()
-				-- configure lua server (with special settings)
 				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
+					-- Command and arguments to start the server.
+					cmd = { "lua-language-server" },
+					-- Filetypes to automatically attach to.
+					filetypes = { "lua" },
+					-- Sets the "root directory	-- Sets the "root directory" to the parent directory of the file in the
+					-- current buffer that contains either a ".luarc.json" or a
+					-- ".luarc.jsonc" file. Files that share a root directory will reuse
+					-- the connection to the same LSP server.
+					root_markers = { ".luarc.json", ".luarc.jsonc" },
 					settings = {
 						Lua = {
-							-- make the language server recognize "vim" global
+							runtime = {
+								version = "LuaJIT",
+							},
 							diagnostics = {
 								globals = { "vim" },
+							},
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
 							},
 							completion = {
 								callSnippet = "Replace",
