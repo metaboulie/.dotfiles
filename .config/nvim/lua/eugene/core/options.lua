462de82f7ed8919670cd5c50484a57268feebecb
metaboulie <eugeneheiner14@gmail.com>
2025-03-04 15:22:48 +0800

    Remove a incorrect comment

--- .config/nvim/lua/eugene/core/options.lua
+++ .config/nvim/lua/eugene/core/options.lua
@@ -17,7 +17,7 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = true -- disable line wrapping
+opt.wrap = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
