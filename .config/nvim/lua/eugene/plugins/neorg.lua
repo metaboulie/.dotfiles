fce08a23dffa72d4e4c59803f0fd3eac7e8d3982
metaboulie <eugeneheiner14@gmail.com>
2025-03-12 16:00:42 +0800

    Modifying neovim everyday

--- .config/nvim/lua/eugene/plugins/neorg.lua
+++ .config/nvim/lua/eugene/plugins/neorg.lua
@@ -5,7 +5,6 @@ return {
 		version = "*",
 		config = function()
 			local neorg = require("neorg")
-
 			neorg.setup({
 				load = {
 					["core.defaults"] = { config = { disable = { "core.pivot" } } },
@@ -30,32 +29,18 @@ return {
 					["core.summary"] = {},
 				},
 			})
-
 			vim.wo.foldlevel = 99
 			vim.wo.conceallevel = 2
-
 			-- set keymaps
 			local keymap = vim.keymap
-
 			keymap.set("n", "<leader>njt", ":Neorg journal today<CR>", {
 				desc = "Open journal for today",
-			})
-			keymap.set("n", "<leader>njy", ":Neorg journal yesterday<CR>", {
-				silent = true,
-				noremap = true,
-				desc = "Open journal for yesterday",
-			})
-			keymap.set("n", "<leader>njm", ":Neorg journal tomorrow<CR>", {
-				silent = true,
-				noremap = true,
-				desc = "Open journal for tomorrow",
 			})
 			keymap.set("n", "<leader>njo", ":Neorg journal toc open<CR>", {
 				silent = true,
 				noremap = true,
 				desc = "Open journal table of contents",
 			})
-
 			-- Binding Keys for Norg Files Only
 			vim.api.nvim_create_autocmd("Filetype", {
 				pattern = "norg",
