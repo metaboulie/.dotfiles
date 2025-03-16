63b2993b45c6a785268845452c6b793201594914
metaboulie <eugeneheiner14@gmail.com>
2025-03-17 00:46:15 +0800

    ℹ

--- .config/nvim/lua/eugene/plugins/formatter.lua
+++ .config/nvim/lua/eugene/plugins/formatter.lua
@@ -4,18 +4,10 @@ return {
 	event = { "BufReadPre", "BufNewFile", "BufWritePre" },
 	config = function()
 		local conform = require("conform")
-
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				graphql = { "prettier" },
-				liquid = { "prettier" },
 				lua = { "stylua" },
 				python = { "ruff" },
 				rust = { "rustfmt" },
@@ -28,13 +20,5 @@ return {
 				timeout_ms = 1000,
 			},
 		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
--- .config/nvim/lua/eugene/plugins/linter.lua
+++ .config/nvim/lua/eugene/plugins/linter.lua
@@ -18,9 +18,5 @@ return {
 				lint.try_lint()
 			end,
 		})
-
-		vim.keymap.set("n", "<leader>lll", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
 	end,
 }
