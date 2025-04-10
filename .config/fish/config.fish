41fbffecf73f682bc633f54575ae6c6606b90283
metaboulie <eugeneheiner14@gmail.com>
2025-04-10 14:50:53 +0800

    fafafafafafafafa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,69 +5,50 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.ghcup/bin
 fish_add_path $HOME/opt/bin
 
-# set default editor 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-
 set -gx HATCH_CONFIG ~/.config/hatch.toml
-
-# rust
 set -gx PATH $HOME/.cargo/bin $PATH
 
-# node
 function nvm
   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
-# zoxide
 zoxide init fish | source
-
-# fzf
 fzf --fish | source
 
-# eza
 alias ls 'eza --all --header --icons --git --git-ignore --tree --long --git-repos-no-status --binary --total-size'
 alias lsa 'eza --all --header --icons --git --tree --long --git-repos-no-status --binary --total-size'
 alias lss 'eza'
 alias lsss 'eza -R -l -L 2'
 alias lsbin 'eza ~/opt/bin -l --no-permissions --no-user -U --no-filesize'
 
-# btop
 alias bt "btop -t -lc"
 
-# builtin
 alias c 'clear'
 alias a "open -a 'Arc'"
 alias credit 'z ~/metaboulie; v credits.txt'
 alias clean 'brew autoremove; brew cleanup --prune=all'
 
-# update package managers
 alias u 'brew upgrade; rustup update; uv tool upgrade --all'
 
-# fish
 alias nf 'nvim ~/.config/fish/config.fish'
-alias ng 'nvim ~/.gitconfig'
 alias sf 'source ~/.config/fish/config.fish'
 
-# neovim
 alias nn 'z ~/.config/nvim/; nvim .'
 alias v 'nvim'
 alias vl 'nvim ~/.local/state/nvim/lsp.log'
 alias nc 'z ~/.config; nvim .'
 alias n 'z ~/metaboulie; nvim todo.norg'
 
-# brew
 alias bi 'brew info'
 alias bh 'brew home'
 alias bl 'brew list'
 alias bd 'brew doctor'
-alias bc 'brew cleanup'
 alias bu 'brew uses --recursive --installed'
 
-# git
 alias gs 'git switch'
 alias gsm 'git switch main'
-alias gsd 'git switch dev'
 alias gc 'git clone --depth=1'
 alias gf 'git fetch --prune'
 alias gm 'git merge --no-ff'
@@ -76,22 +57,17 @@ alias gbd 'git branch -D'
 alias gl 'git ls-remote'
 alias gbe 'git branch --edit-description'
 
-# marimo
 alias me 'uv run marimo edit --no-token'
 alias md 'uv run marimo -d edit --no-token --headless'
 
-# uv
 alias ue 'uv pip install -e .'
 
-# gh
 alias ghd "gh release download --clobber --dir ~/opt"
 
-# ruff
 alias rc 'ruff check'
 alias rf 'ruff format'
 alias r 'ruff format; ruff check'
 
-# bat
 function help -d 'print help message'
   # Execute command in a new fish process to ensure fish builtins work correctly
   # fish -c evaluates the following string as fish code in a new process
@@ -103,11 +79,9 @@ function df -d 'show changes lines for all files in pwd'
   fd . --type file | xargs bat --diff 
 end
 
-# use bat as man's pager
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx MANROFFOPT "-c"
 
-# yazi
 function y -d 'open yazi'
   set tmp (mktemp -t "yazi-cwd.XXXXXX")
   yazi $argv --cwd-file="$tmp"
@@ -117,7 +91,6 @@ function y -d 'open yazi'
   rm -f -- "$tmp"
 end
 
-# hgrep
 function hg -d 'prettier regex search'
   command hgrep --no-grid --printer bat -S "$argv" | less -R
 end
