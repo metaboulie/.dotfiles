7c8ee9dd0189b12d997eec81155a6f90a9829af6
métaboulie <heinereugene@pm.me>
2025-04-15 12:23:01 +0800

    neovim: add prettierd as formatter for html and css

--- .config/nvim/lua/eugene/plugins/formatter.lua
+++ .config/nvim/lua/eugene/plugins/formatter.lua
@@ -12,6 +12,8 @@ return {
 				toml = { "taplo" },
 				haskell = { "fourmolu" },
 				python = { "ruff" },
+				html = { "prettierd" },
+				css = { "prettierd" },
 			},
 			format_on_save = { lsp_fallback = true, async = false, timeout_ms = 1000 },
 		})
