abdf93bdabe0091ba364d75860863bc5f69c09cb
métaboulie <heinereugene@pm.me>
2025-04-24 12:03:05 +0800

    further cleaning neovim

--- .config/nvim/lua/eugene/plugins/init.lua
+++ .config/nvim/lua/eugene/plugins/init.lua
@@ -32,4 +32,24 @@ return {
 			surround.setup({})
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = { "windwp/nvim-ts-autotag" },
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
+			treesitter.setup({
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				ensure_installed = { "lua" },
+				ignore_install = { "org" },
+				indent = { enable = true },
+				autotag = { enable = true },
+				enable = true,
+				modules = {},
+			})
+		end,
+	},
 }
