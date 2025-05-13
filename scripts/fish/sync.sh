9ce86fedf4337b7aca463496984a4830b7ad1716
métaboulie <heinereugene@pm.me>
2025-05-13 14:22:00 +0800

    update: 13/05/25

--- scripts/fish/sync.sh
+++ scripts/fish/sync.sh
@@ -1,16 +1,19 @@
 #!/opt/homebrew/bin/fish
 # automatically z to different workspaces, commit and push daily updates
 
-function git_daily_update
+function git_sync
     z $argv[1]; or exit
+    set_color orange 
     echo "syncing $argv[1]"
+    set_color normal
     git add .
     git commit -m "update: $(date "+%d/%m/%y")"
     git push; or exit
+    set_color orange 
     echo "syncing finished"
     z -
 end
 
-git_daily_update ~/deepBoredom
-git_daily_update ~/metaboulie
-git_daily_update ~
+git_sync ~/deepBoredom
+git_sync ~/metaboulie
+git_sync ~
