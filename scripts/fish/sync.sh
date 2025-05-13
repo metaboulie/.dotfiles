0bd3de2c3d317ac6863a9df656e3f10d698ad0d8
métaboulie <heinereugene@pm.me>
2025-05-13 14:22:59 +0800

    update: 13/05/25

--- scripts/fish/sync.sh
+++ scripts/fish/sync.sh
@@ -3,13 +3,13 @@
 
 function git_sync
     z $argv[1]; or exit
-    set_color orange 
+    set_color bryellow 
     echo "syncing $argv[1]"
     set_color normal
     git add .
     git commit -m "update: $(date "+%d/%m/%y")"
     git push; or exit
-    set_color orange 
+    set_color bryellow 
     echo "syncing finished"
     z -
 end
